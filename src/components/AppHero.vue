b3a5d030a2f1056c79bdd79ff22918b9ab975a7d
gds90 <you@example.com>
2023-12-21 16:50:46 +0100

    Aggiunto Bonus Jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+import AppTitle from './AppTitle.vue';
+export default {
+    components: {
+        AppTitle,
+    }
+}
+</script>
+<template lang="">
+    <div class="jumbotron mb-4 d-flex align-items-center">
+        <div class="container-fluid">
+          <h1 class="display-5 fw-bold"><AppTitle /></h1>
+        </div>
+    </div>
+</template>
+<style>
+.jumbotron {
+    background-image: url('https://s3.amazonaws.com/codecademy-content/projects/junction/bg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 60vh;
+    color: #fff;
+}
+
+h1 {
+    opacity: 0.9;
+}
+</style>
