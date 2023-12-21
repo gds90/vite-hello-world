24a3476212b61534a4b33aa5192fdfa58463b0d7
gds90 <you@example.com>
2023-12-21 14:58:27 +0100

    Aggiunta, importazione e utilizzo del componente AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    },
+
+}
+</script>
+<template lang="">
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+<style lang="">
+    
+</style>
